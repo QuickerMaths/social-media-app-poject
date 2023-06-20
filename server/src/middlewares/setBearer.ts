a8b19d0aa21aeb438f5c1e71d066e1f7b25f7eb6
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-06-20 14:25:45 +0200

    fixed setBearer middleware

--- server/src/middlewares/setBearer.ts
+++ server/src/middlewares/setBearer.ts
@@ -1,7 +1,6 @@
 export const setBearer = async (req: any, res: any, next: any) => {
   if (req.url.startsWith("/api")) {
     const cookie = req.cookies[`${process.env.FRONTEND_DOMAIN}`];
-    if (!cookie) return;
 
     req.headers.authorization = `Bearer ${cookie}`;
   }
