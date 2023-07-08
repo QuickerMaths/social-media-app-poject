6c66d2e5ab41fbce53f29b676846ed000b2ec6cd
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-07-09 00:58:56 +0200

    added styles to friendsRequestList

--- client/src/components/friends-request/FriendsRequest.tsx
+++ client/src/components/friends-request/FriendsRequest.tsx
@@ -1,12 +1,33 @@
 import React from "react";
-import { IUser } from "../../pages/user-profile/types";
+import { useAppDispatch } from "../../hooks/reduxHooks";
+import { IUserBasicData } from "../../pages/user-profile/types";
+import PostOwner from "../post/sumcomponents/PostOwner";
 
 interface Props {
-  request: IUser;
+  request: IUserBasicData;
+  setReRender: React.Dispatch<React.SetStateAction<boolean>>;
+  reRender: boolean;
 }
 
-const FriendsRequest: React.FC<Props> = ({ request }) => {
-  return <div>FriendsRequest</div>;
+const FriendsRequest: React.FC<Props> = ({
+  request,
+  setReRender,
+  reRender,
+}) => {
+  const dispatch = useAppDispatch();
+  return (
+    <li className="request">
+      <PostOwner owner={request} />
+      <div className="request__button-container">
+        <button className="request__button request__button--accept">
+          Accept
+        </button>
+        <button className="request__button request__button--decline">
+          Decline
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default FriendsRequest;
