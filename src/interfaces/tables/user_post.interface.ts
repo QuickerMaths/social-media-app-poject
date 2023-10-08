043efa7c26f3dfa9e03ecf7a53eb430ea910f54b
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-10-08 21:23:05 +0200

    fix: refactored user_post interface

--- src/interfaces/tables/user_post.interface.ts
+++ src/interfaces/tables/user_post.interface.ts
@@ -4,10 +4,14 @@ export default interface IUserPost {
   id: number;
   profile_id: Pick<IUserProfile, "id">;
   shared_post_id: Pick<IUserPost, "id"> | null;
-  post_text?: string;
-  media_location?: string;
+  post_text?: string | null;
+  media_location?: string | null;
+  shared_post_text?: string | null;
+  shared_media_location?: string | null;
   share_count: number;
   comment_count: number;
   is_shared: boolean;
   createdAt: Date;
 }
+
+//TODO: write some code that will be responsible for handling share_count and comment_count
