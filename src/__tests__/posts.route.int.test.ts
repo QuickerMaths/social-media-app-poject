56f427967436fdb467cc7f00b0d8e4084888139e
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-10-22 20:32:44 +0200

    feat: jest setup

---
+++ src/__tests__/posts.route.int.test.ts
@@ -0,0 +1,13 @@
+describe("Posts route integration tests", () => {
+  it("should return all posts", () => {
+    expect(1).toBe(1);
+  });
+
+  it("should return a post by id", () => {
+    expect(1).toBe(1);
+  });
+
+  it("should return all posts by user id", () => {
+    expect(1).toBe(1);
+  });
+});
