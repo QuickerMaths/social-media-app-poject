a7ae174b245629de248a28a7f999ec9391293c8a
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-11-16 15:28:43 +0100

    feat: separated comment form into CommentInput component

---
+++ client/src/components/comment-input/CommentInput.tsx
@@ -0,0 +1,64 @@
+// External dependencies
+
+import React from "react";
+import { useFormik } from "formik";
+
+//Internal dependencies
+
+import Spinner from "../../utilities/spinner/Spinner";
+import useToastCreator from "../../hooks/useToastCreator";
+import { IPost } from "../post/types";
+import { useCreateCommentMutation } from "../../features/apiSlice/commentApiSlice/commentApiSlice";
+
+interface Props {
+  post: IPost;
+}
+
+const CommentInput: React.FC<Props> = ({ post }) => {
+  const { id } = post;
+  const [createComment, { isLoading: isUpdating, error, isError }] =
+    useCreateCommentMutation();
+
+  const { handleSubmit, values, handleChange } = useFormik({
+    initialValues: {
+      comment_text: "",
+    },
+    onSubmit: async ({ comment_text }) => {
+      await createComment({
+        comment_text,
+        id,
+      });
+      if (isError) useToastCreator(error as string, "error");
+      if (!isUpdating && !isError) {
+        comment_text = "";
+      }
+    },
+  });
+
+  let content;
+
+  if (isUpdating) {
+    content = <Spinner size={50} />;
+  } else {
+    content = (
+      <form onSubmit={handleSubmit} className="post-details-modal__form">
+        <input
+          name="comment_text"
+          id="comment_text"
+          value={values.comment_text}
+          onChange={handleChange}
+          className="post-details-modal__text-area"
+          placeholder="Write a comment..."
+        />
+        <button className="post-details-modal__submit-button" type="submit">
+          Comment
+        </button>
+      </form>
+    );
+  }
+  return (
+    <div className="post-details-modal__comment-input-container">{content}</div>
+  );
+};
+
+export default CommentInput;
--- client/src/portals/post-details-modal/PostDetailsModal.tsx
+++ client/src/portals/post-details-modal/PostDetailsModal.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import moment from "moment";
 import { AiOutlineClose } from "react-icons/ai";
-import { useFormik } from "formik";
 
 // Internal dependencies
 
@@ -13,66 +12,21 @@ import PostComments from "../../components/post/subcomponents/PostComments";
 import PostOwner from "../../components/post/subcomponents/PostOwner";
 import PostDetailsPost from "./subcomponents/PostDetailsPost";
 import PostDetailsRePost from "./subcomponents/PostDetailsRePost";
-import Spinner from "../../utilities/spinner/Spinner";
-import useToastCreator from "../../hooks/useToastCreator";
+import CommentInput from "../../components/comment-input/CommentInput";
 import { IPost } from "../../components/post/types";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import { RootState } from "../../redux/store";
 import { closeModal } from "../../features/modalSlice/modalSlice";
-import { useCreateCommentMutation } from "../../features/apiSlice/commentApiSlice/commentApiSlice";
 
 interface Props {
   post: IPost;
 }
 
 const PostDetailsModal: React.FC<Props> = ({ post }) => {
-  const [createComment, { isLoading: isUpdating, error, isError }] =
-    useCreateCommentMutation();
-
   const { id, created_at, comments, comment_count, post_owner, is_shared } =
     post;
   const dispatch = useAppDispatch();
-  const { userId } = useAppSelector((state: RootState) => state.auth);
   const { modals } = useAppSelector((state: RootState) => state.modal);
-
-  const { handleSubmit, values, handleChange } = useFormik({
-    initialValues: {
-      comment_text: "",
-    },
-    onSubmit: async ({ comment_text }) => {
-      await createComment({
-        comment_text,
-        id,
-        userId: userId as number,
-      });
-      if (isError) useToastCreator(error as string, "error");
-      if (!isUpdating && !isError) {
-        comment_text = "";
-      }
-    },
-  });
-
-  let content;
-
-  if (isUpdating) {
-    content = <Spinner size={50} />;
-  } else {
-    content = (
-      <form onSubmit={handleSubmit} className="post-details-modal__form">
-        <input
-          name="comment_text"
-          id="comment_text"
-          value={values.comment_text}
-          onChange={handleChange}
-          className="post-details-modal__text-area"
-          placeholder="Write a comment..."
-        />
-        <button className="post-details-modal__submit-button" type="submit">
-          Comment
-        </button>
-      </form>
-    );
-  }
 
   if (!modals[`${id}details`]) return null;
   return ReactDOM.createPortal(
@@ -104,7 +58,7 @@ const PostDetailsModal: React.FC<Props> = ({ post }) => {
           <PostAction post={post} />
           {comment_count > 0 && <PostComments comments={comments} />}
         </div>
-        {content}
+        <CommentInput post={post} />
       </div>
     </div>,
     document.body
